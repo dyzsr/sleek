41fec1ebd7721a1b8c2932eb3a33db666a541dc7
songyahui <songyahuiapp@hotmail.com>
2021-04-13 02:12:33 +0800

    added tow more methods in Signals

--- signals.mli
+++ signals.mli
@@ -3,12 +3,18 @@ type event
 
 val show_event : event -> string
 
+val compare_event: event -> event -> bool
+
+
+
 val present : string -> event
 
 val is_present : event -> bool
 
 (* Type of signals *)
 type t
+
+val isSigOne: event -> t -> bool
 
 val show : t -> string
 
