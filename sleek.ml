5e3a4d6956ccbdd125f281a24fddd378a894dd02
dongyan <dybest@icloud.com>
2021-05-02 02:49:31 +0800

    Update sleek interface

--- sleek.ml
+++ sleek.ml
@@ -1,4 +1,16 @@
 include Ast
+include Ast_utils
 include Syntax
 include Verifier
 include Utils
+module Ast = Ast
+module Ast_utils = Ast_utils
+module Checker = Checker
+module Colors = Colors
+module History = History
+module Inference = Inference
+module Proofctx = Proofctx
+module Signals = Signals
+module Syntax = Syntax
+module Utils = Utils
+module Verifier = Verifier
